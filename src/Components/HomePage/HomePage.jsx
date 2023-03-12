d41df3f56e9bfb515279669ebfae8f28ca2ee056
vzhev <v.zhevaga@gmail.com>
2023-03-12 21:32:40 +0100

    added homepage

---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,37 @@
+import AccentButton from './../AccentButton/AccentButton';
+import s from './HomePage.module.css';
+import icecream from './media/icecream.png';
+// import icecream from './media/preview.png';
+
+const HomePage = () => {
+	return (
+		<div className={s.homepage}>
+			<div className="container">
+				<div className={s.wrapper}>
+					<div className={s.offer}>
+						<p className={s.subtitle}>Sweet fun, full of milk.</p>
+						<h1 className={s.title}>
+							Feel inside cold with <br />
+							our delicious <span>ice-cream.</span>
+						</h1>
+						<p className={s.substring}>
+							Some food has looked so awful that it's looked like something that
+							the dog's brought home, yet after one mouthful I've been left
+							eating my thoughts, my words.
+						</p>
+						<AccentButton text="Buy Now" />
+					</div>
+					<div className={s.image}>
+						<img
+							className={s.img}
+							src={icecream}
+							alt="picture icecream in glass"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default HomePage;
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,7 +1,12 @@
+import HomePage from '../HomePage/HomePage';
 import s from './Main.module.css';
 
 const Main = () => {
-	return <main className={s.main}></main>;
+	return (
+		<main className={s.main}>
+			<HomePage />
+		</main>
+	);
 };
 
 export default Main;
